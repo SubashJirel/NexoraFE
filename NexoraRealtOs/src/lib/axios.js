1e9fafee1e8b02e26db000c90e3fb5ba27bc450c
Subash Jirel <subashjrl@gmail.com>
2026-07-18 07:41:07 +0545

    env check

--- NexoraRealtOs/src/lib/axios.js
+++ NexoraRealtOs/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import { useAuthStore } from '@/store/authStore'
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api'
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
+console.log('BASE_URL',BASE_URL)
 const REFRESH_URL = '/auth/token/refresh/'
 
 export const apiClient = axios.create({
