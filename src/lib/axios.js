7260ffd1bc139c455e5e29bcdc377b2ca0ae7a95
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2026-08-04 20:19:52 +0545

    skip browser

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -9,6 +9,7 @@ export const apiClient = axios.create({
   baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
+    "ngrok-skip-browser-warning": "true",
   },
   timeout: 15000,
 })
